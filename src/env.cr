f6c3ca28f6682935a04129593bcb83d75c50de16
reiswindy <megamanbn@gmail.com>
2018-05-28 13:35:30 -0500

    Use load! to force file existence

--- src/env.cr
+++ src/env.cr
@@ -2,5 +2,5 @@ ENV["APP_ENV"] ||= "development"
 
 {% if env("APP_ENV") != "production"%}
   require "dotenv"
-  Dotenv.load(".env.#{ENV["APP_ENV"]}")
+  Dotenv.load! ".env.#{ENV["APP_ENV"]}" # File must exist in non-production!
 {% end %}
