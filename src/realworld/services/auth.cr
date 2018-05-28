60d362ba46370de32b535e0751571103c6473df5
reiswindy <megamanbn@gmail.com>
2018-05-28 16:22:05 -0500

    Add Auth

--- src/realworld/services/auth.cr
+++ src/realworld/services/auth.cr
@@ -1,4 +1,19 @@
+require "jwt"
+require "./repo"
+
 module Realworld::Services
   class Auth
+    Algorithm = ENV["JWT_ALGORITHM"]
+    Secret = ENV["JWT_SECRET"]
+
+    def self.auth(token)
+      begin
+        payload, header = JWT.decode(token, Secret, Algorithm)
+        id = payload.as(Hash(String, JSON::Type))["id"].as(Int64)
+        user = Repo.get!(User, id)
+      rescue exception
+        nil
+      end
+    end
   end
 end
