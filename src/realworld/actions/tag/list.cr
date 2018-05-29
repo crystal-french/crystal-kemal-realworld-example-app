bbb57d8b6fa71cabe10c804acfe35bb7198f3bbb
reiswindy <megamanbn@gmail.com>
2018-05-29 17:22:31 -0500

    Fix, use correct class

--- src/realworld/actions/tag/list.cr
+++ src/realworld/actions/tag/list.cr
@@ -1,10 +1,11 @@
 require "../base"
+require "../../models/tag"
 require "../../services/repo"
 
 module Realworld::Actions::Tag
   class List < Realworld::Actions::Base
     def call(env)
-      tags = Realworld::Services::Repo.all(Tag)
+      tags = Realworld::Services::Repo.all(Realworld::Models::Tag)
       # TODO: Return success
     end
   end  
