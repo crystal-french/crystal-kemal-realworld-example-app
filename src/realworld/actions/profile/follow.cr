384817fd3a2a0dfc07ab66f80f7617e65ab6d331
reiswindy <megamanbn@gmail.com>
2018-05-29 17:46:23 -0500

    Add profile action stub

--- src/realworld/actions/profile/follow.cr
+++ src/realworld/actions/profile/follow.cr
@@ -1,9 +1,31 @@
 require "../base"
+require "../../models/user"
+require "../../models/following"
+require "../../services/repo"
 
 module Realworld::Actions::Profile
   class Follow < Realworld::Actions::Base
     def call(env, user)
-      # TODO: Logic
+      if user
+        p_owner = Realworld::Services::Repo.get_by(Realworld::Models::User, username: env.params.url["username"])
+        if p_owner
+          if user.followed_users.select {|fu| fu.followed_user_id == p_owner.id}.size = 0
+            following = Realworld::Models::Following.new
+            following.follower_user_id = user.id
+            following.followed_user_id = p_owner.id
+
+            Realworld::Services::Repo.insert(following)
+
+            user.followed_users << following
+          end
+
+          # TODO: Return success
+        else
+          # TODO: Return error
+        end
+      else
+        # TODO: Return error
+      end
     end
   end
 end
--- src/realworld/actions/profile/get.cr
+++ src/realworld/actions/profile/get.cr
@@ -1,9 +1,17 @@
 require "../base"
+require "../../models/user"
+require "../../services/repo"
 
 module Realworld::Actions::Profile
   class Get < Realworld::Actions::Base
     def call(env, user)
-      # TODO: Logic
+      owner = Realworld::Services::Repo.get_by(Realworld::Models::User, username: env.params.url["username"])
+      if owner
+        # TODO: user.to_profile
+        # TODO: Return success
+      else
+        # TODO: Return error
+      end
     end
   end
 end
