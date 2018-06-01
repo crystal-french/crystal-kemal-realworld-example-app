9358a84b97bf6ca204e4b87ecd7151337a0a10b4
reiswindy <megamanbn@gmail.com>
2018-06-01 13:38:25 -0500

    Add error handlers

--- src/realworld.cr
+++ src/realworld.cr
@@ -3,6 +3,7 @@ require "./realworld/models/*"
 require "./realworld/services/*"
 require "./realworld/handlers/*"
 require "./realworld/version"
+require "./realworld/errors"
 require "./realworld/routes"
 
 require "kemal"
@@ -15,7 +16,11 @@ add_handler(Realworld::Handlers::AuthOptionalHandler.new)
 error 401 {|env| ""}
 error 403 {|env| ""}
 error 404 {|env| ""}
-error 422 {|env, exception| exception.to_s}
+
+error 422 do |env, exception| 
+  errors = exception.as(Realworld::UnprocessableEntityException).content
+  {"errors" => errors}.to_json
+end
 
 before_all {|env| env.response.content_type = "application/json"}
 
---
+++ src/realworld/errors.cr
@@ -0,0 +1,31 @@
+module Realworld
+  class UnauthorizedException < Kemal::Exceptions::CustomException
+    def initialize(context)
+      context.response.status_code = 401
+      super(context)
+    end
+  end
+
+  class ForbiddenException < Kemal::Exceptions::CustomException
+    def initialize(context)
+      context.response.status_code = 403
+      super(context)
+    end
+  end
+
+  class NotFoundException < Kemal::Exceptions::CustomException
+    def initialize(context)
+      context.response.status_code = 404
+      super(context)
+    end    
+  end
+
+  class UnprocessableEntityException < Kemal::Exceptions::CustomException
+    def initialize(context, @content = {} of String => Array(String))
+      context.response.status_code = 422
+      super(context)
+    end
+
+    getter :content
+  end
+end
