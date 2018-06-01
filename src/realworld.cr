627117d772e37e85f55d15bef667e1826127adf5
reiswindy <megamanbn@gmail.com>
2018-06-01 10:59:23 -0500

    Add error handlers

--- src/realworld.cr
+++ src/realworld.cr
@@ -12,8 +12,11 @@ add_context_storage_type(Realworld::Models::User?)
 add_handler(Realworld::Handlers::AuthRequiredHandler.new)
 add_handler(Realworld::Handlers::AuthOptionalHandler.new)
 
-before_all do |env|
-  env.response.content_type = "application/json"
-end
+error 401 {|env| ""}
+error 403 {|env| ""}
+error 404 {|env| ""}
+error 422 {|env, exception| exception.to_s}
+
+before_all {|env| env.response.content_type = "application/json"}
 
 Kemal.run
